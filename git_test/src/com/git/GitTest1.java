c91cbfdec19557d2dac3ebf225c7ad4788221d20
admin <1353248575@qq.com>
2019-06-17 19:12:46 +0800

    修改1

--- git_test/src/com/git/GitTest1.java
+++ git_test/src/com/git/GitTest1.java
@@ -3,7 +3,10 @@ package com.git;
 public class GitTest1 {
     public static void main(String[] args) {
 
-
+        System.out.println("初始化项目1");
+        System.out.println("初始化项目1");
+        System.out.println("初始化项目1");
+        System.out.println("初始化项目1");
         System.out.println("初始化项目1");
     }
 }
